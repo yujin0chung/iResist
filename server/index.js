85d8bf75fb945cf5f014603ed9446cc659ad89f8
Alex Seiden <alex.seiden@gmail.com>
2017-05-27 16:20:37 -0700

    env monitoring

--- server/index.js
+++ server/index.js
@@ -4,5 +4,6 @@ const db = require('../db');
 const PORT = process.env.port || 3000;
 
 app.listen(PORT, () => {
+  console.log(process.env.port);
   console.log('Example app listening on port: ', PORT);
 });
