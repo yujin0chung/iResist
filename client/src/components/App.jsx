3538efc3568b4d8acdf7e410349e0b7ad7d6be93
Alex Seiden <alex.seiden@gmail.com>
2017-05-31 16:26:30 -0700

    (feat) render app in index.js

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor (props) {
+    super(props);
 
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Hello from the app!</h1>;
+  }
 }
 
 export default App;
