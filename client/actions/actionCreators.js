13cf1058d07c901025a9db208a6ac0d3a67179be
Alex Seiden <alex.seiden@gmail.com>
2017-05-30 19:12:55 -0700

    load actions in actionCreators.js

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -3,3 +3,12 @@ import feedActions from './feedActions';
 import mapActions from './mapActions';
 import navActions from './navActions';
 
+// export event actions
+export const createEvent = eventActions.createEvent;
+export const editEvent = eventActions.editEvent;
+export const deleteEvent = eventActions.deleteEvent;
+export const attendEvent = eventActions.attendEvent;
+
+// navigation actions
+export const viewDashboard = navActions.viewDashboard;
+export const viewCreateEditEvent = navActions.viewCreateEditEvent;
