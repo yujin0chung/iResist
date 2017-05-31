fd39735c1fd826f3e470d995655201df8bc4e525
Alex Seiden <alex.seiden@gmail.com>
2017-05-30 18:59:39 -0700

    specific stubs for actions, nav, map, and feed

---
+++ client/actions/feedActions.js
@@ -0,0 +1,20 @@
+export const postItem = () => {
+  return {
+    type: 'INSERT TYPE HERE',
+    // INSERT OTHER CHANGES HERE
+  };
+};
+
+export const voteItem = () => {
+  return {
+    type: 'INSERT TYPE HERE',
+    // INSERT OTHER CHANGES HERE
+  };
+};
+
+export const deleteItem = () => {
+  return {
+    type: 'INSERT TYPE HERE',
+    // INSERT OTHER CHANGES HERE
+  };
+};
---
+++ client/actions/mapActions.js
@@ -0,0 +1,20 @@
+export const dropPin = () => {
+  return {
+    type: 'INSERT TYPE HERE',
+    // INSERT OTHER CHANGES HERE
+  };
+};
+
+export const votePin = () => {
+  return {
+    type: 'INSERT TYPE HERE',
+    // INSERT OTHER CHANGES HERE
+  };
+};
+
+export const RemovePin = () => {
+  return {
+    type: 'INSERT TYPE HERE',
+    // INSERT OTHER CHANGES HERE
+  };
+};
