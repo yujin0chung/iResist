5363d4d29f6a05854a42f5d0a9301d469cfc0728
Alex Seiden <alex.seiden@gmail.com>
2017-06-01 15:59:49 -0700

    (feat) render additional protest information

--- client/src/components/Protest.jsx
+++ client/src/components/Protest.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import dateFormat from 'dateformat';
 
 class Protest extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      displayDetails: true
+    };
   }
 
   render() {
-    return (
-      <div>
-        <h3>{this.props.protest.name} - {this.props.role}</h3>
-        <p>{this.props.protest.cause}</p>
-        <p>{this.props.protest.description}</p>
-      </div>
-    );
+    const startTime = new Date(this.props.protest.eventStart);
+    const endTime = new Date(this.props.protest.eventStart + this.props.protest.eventDuration);
+
+    console.log('THIS IS THE START TIME', startTime);
+
+    if (this.state.displayDetails) {
+      return (
+        <div>
+          <h3>{this.props.protest.name} - {this.props.role}</h3>
+          <p>Cause: {this.props.protest.cause}</p>
+          <p>Start: {dateFormat(startTime, 'mmmm dd, yyyy: hh:mm')}</p>
+          <p>End: {dateFormat(endTime, 'mmmm dd, yyyy: hh:mm')}</p>
+          <p>Address: {this.props.protest.address}</p>
+          <p>Description: {this.props.protest.description}</p>
+          <p>Attendee Count: {this.props.protest.attendee_count}</p>
+        </div>
+      );
+    }
   }
 }
 
