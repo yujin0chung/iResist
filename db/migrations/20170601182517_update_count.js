553ad5543380c46655463fec5d88cc13ce50b66c
Yujin Chung <yujin0chung@gmail.com>
2017-06-01 19:21:42 -0700

    Update 20170601182517_update_count.js

--- db/migrations/20170601182517_update_count.js
+++ db/migrations/20170601182517_update_count.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
     t.string('address', 200).nullable();
     t.integer('attendee_count').nullable();
     t.integer('time').nullable();
-    t.integer('durration').nullable();
+    t.integer('duration').nullable();
     t.integer('latitude').nullable();
     t.integer('longitude').nullable();
   })
@@ -15,7 +15,7 @@ exports.down = function(knex, Promise) {
     t.dropColumn('address', 200);
     t.dropColumn('attendee_count');
     t.dropColumn('time');
-    t.dropColumn('durration');
+    t.dropColumn('duration');
     t.dropColumn('latitude');
     t.dropColumn('longitude');
   })
