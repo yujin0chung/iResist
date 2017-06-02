fe7a626a725b6d7f074fad29405edec1606aa643
Russ Ward <maddogward@gmail.com>
2017-06-01 19:10:20 -0700

    Add migration files to add count, time, durration, lat, and long, remove event_time

---
+++ db/migrations/20170601182517_update_count.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('events', function(t) {
+    t.string('address', 200).nullable();
+    t.integer('attendee_count').nullable();
+    t.integer('time').nullable();
+    t.integer('durration').nullable();
+    t.integer('latitude').nullable();
+    t.integer('longitude').nullable();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('events', function(t) {
+    t.dropColumn('address', 200);
+    t.dropColumn('attendee_count');
+    t.dropColumn('time');
+    t.dropColumn('durration');
+    t.dropColumn('latitude');
+    t.dropColumn('longitude');
+  })
+};
