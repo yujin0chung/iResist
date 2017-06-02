bbad943b3d72bfc6ce61e7673a0f10a791cb650a
Alex Seiden <alex.seiden@gmail.com>
2017-06-01 20:58:52 -0700

    (feaT) render multiple pins

--- client/src/components/MapContainer.jsx
+++ client/src/components/MapContainer.jsx
@@ -3,18 +3,22 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import l from 'leaflet';
 
 const MapContainer = (props) => {
-  const position = [51.505, -0.09];
+  const positions = [[51.505, -0.09], [52.505, -0.09], [53.505, -0.09]];
   const map = (
-    <Map className='dashboardMap' center={position} zoom={13}>
+    <Map className='dashboardMap' center={positions[0]} zoom={13}>
       <TileLayer
         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
-        <Popup>
-          <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-        </Popup>
-      </Marker>
+      { positions.map((position) => {
+        return (
+          <Marker position={position} draggable={true}>
+            <Popup>
+              <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+            </Popup>
+        </Marker>
+        );
+      }) }
     </Map>
   );
   return map;
