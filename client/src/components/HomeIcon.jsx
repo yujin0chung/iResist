3deb8151581cc3e35c0ec37614e8c20a8aa948c0
yujin0chung <yujin0chung@gmail.com>
2017-06-02 17:22:23 -0700

    Places HomeIcon in separate file

---
+++ client/src/components/HomeIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const HomeIcon = styled.i`
+  padding-left: 3px;
+  padding-bottom: 10px;
+  color: tomato;
+  aria-hidden: true;
+`;
+
