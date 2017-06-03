814fbb9a719cf1ca617c6ed12a0748618fd79bf4
Alex Seiden <alex.seiden@gmail.com>
2017-06-03 10:50:34 -0700

    (feat) instantiate geocoder

--- server/controllers/geocoder.js
+++ server/controllers/geocoder.js
@@ -1,3 +1,14 @@
+const NodeGeocoder = require('node-geocoder');
+
+const options = {
+  provider: 'openstreetmap',
+  httpAdapter: 'https',
+  formatter: null
+};
+
+const geocoder = NodeGeocoder(options);
+
 module.exports.geocode = (req, res) => {
+
   res.send(200, 'you hit the endpoint!');
 };
