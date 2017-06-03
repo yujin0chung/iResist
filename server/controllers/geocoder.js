4ebb5a7a06af61f945ddd81c290f3ceab4904b8c
Alex Seiden <alex.seiden@gmail.com>
2017-06-03 10:19:08 -0700

    (feat) add geocoder route and controller stub on server

---
+++ server/controllers/geocoder.js
@@ -0,0 +1,3 @@
+module.exports.geocode = (req, res) => {
+  res.send(200, 'you hit the endpoint!');
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,4 +2,5 @@ module.exports.api = require('./api');
 module.exports.auth = require('./auth');
 module.exports.profiles = require('./profiles');
 module.exports.dashboard = require('./dashboard');
-module.exports.events = require('./events')
+module.exports.events = require('./events');
+module.exports.geocoder = require('./geocoder');
