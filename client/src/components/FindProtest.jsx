30721721826f6371a1ef560fea2d1bb91a818e13
yujin0chung <yujin0chung@gmail.com>
2017-06-02 17:03:54 -0700

    Adds home icon which redirects to Dashboard upon click

--- client/src/components/FindProtest.jsx
+++ client/src/components/FindProtest.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import Protest from './Protest.jsx';
+import styled from 'styled-components';
+
+
+const Home = styled.i`
+  padding-left: 3px;
+  padding-bottom: 10px;
+  color: tomato;
+  aria-hidden: true;
+`
 
 class FindProtest extends React.Component {
   render() {
@@ -12,6 +21,11 @@ class FindProtest extends React.Component {
     }
     return (
       <div>
+        <Home 
+          className="fa fa-home"
+          onClick={() => this.props.changeView('DASHBOARD')}
+          >
+        </Home>
         <h3>Find a Protest</h3>
         {
           upcomingEvents.map(protestId => <Protest
