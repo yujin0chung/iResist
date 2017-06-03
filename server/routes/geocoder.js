da2ee1d7700506d8ed742b67b2e240dfa65ed324
Alex Seiden <alex.seiden@gmail.com>
2017-06-03 10:28:56 -0700

    (bug) point geocoder route at controller rather than placeholder

--- server/routes/geocoder.js
+++ server/routes/geocoder.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers').Geocoder;
 
-router.get('/', controllers.SOMETHING);
+router.get('/', controllers.geocode);
 
 module.exports = router;
