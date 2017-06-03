ee61f50a474dcbb7b77b11249dde2b1c80c88a61
yujin0chung <yujin0chung@gmail.com>
2017-06-02 17:46:33 -0700

    Renames HomeIcon.jsx to StyledComponents.jsx and adds Button component

---
+++ client/src/components/StyledComponents.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const HomeIcon = styled.i`
+  padding-left: 3px;
+  padding-bottom: 10px;
+  color: tomato;
+  aria-hidden: true;
+`;
+
+export const Button = styled.div`
+  color: tomato;
+	font-size: 1em;
+  text-align: center;
+	margin: 1em;
+	padding: 0.25em 1em;
+	border: 2px solid tomato;
+	border-radius: 3px;
+`;
