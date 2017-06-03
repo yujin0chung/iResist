d48d377d2ea81d047c2cf30dc42f0a15a3731c1f
yujin0chung <yujin0chung@gmail.com>
2017-06-03 12:09:44 -0700

    Adds cases for JOIN_EVENT, JOIN_ERROR, LEAVE_EVENT, LEAVE_ERROR

--- client/src/reducers/eventReducer.js
+++ client/src/reducers/eventReducer.js
@@ -1,30 +1,36 @@
 export const events = (state = {}, action) => {
-  switch(action.type) {
-    case "RECEIVE_EVENT":
+  switch (action.type) {
+    case 'RECEIVE_EVENT':
       return Object.assign({}, state, {
         event: action.data
       })
-    case "CREATE_ERROR":
+    case 'CREATE_ERROR':
       return Object.assign({}, state, {
         error: action.data
       })
+    case 'JOIN_EVENT':
+      return Object.assign({}, state, {
+        eventId: action.eventId,
+        userId: action.userId
+      })
+    case 'JOIN_ERROR':
+      return Object.assign({}, state, {
+        error: action.error
+      })
+    case 'LEAVE_EVENT': 
+      return Object.assign({}, state, {
+        eventId: action.eventId,
+        userId: action.userId
+      })
+    case 'LEAVE_ERROR':
+      return Object.assign({}, state, {
+        error: action.error
+      })
     default:
       return state;
+  }
+};
 
-// export const eventsReducer = (state = {}, action) => {
-//   switch(action.type) {
-//     case 'RECEIVE_EVENT':
-//       return Object.assign({}, state, {
-//         event: action.data
-//       })
-//     case 'CREATE_ERROR':
-//       return Object.assign({}, state, {
-//         error: action.data
-//       })
-//     default:
-//       return state;
-//   }
-// };
 
 
 
