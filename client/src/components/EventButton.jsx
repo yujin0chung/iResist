095c68bad2aee3a7b43ab3d88c4bb40cad78000d
yujin0chung <yujin0chung@gmail.com>
2017-06-03 09:30:31 -0700

    Debugs EventButton

--- client/src/components/EventButton.jsx
+++ client/src/components/EventButton.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import _ from 'lodash';
 import { AttendButton, AbandonButton } from './StyledComponents.jsx';
 
 const EventButton = (props) => {
+    
   const participating = props.role === 'organizing' || props.role === 'attending';
   if (participating) {
     return (<AbandonButton>Leave</AbandonButton>);
   }
   return (<AttendButton>Join</AttendButton>);
+//   {
+//     participating ? 
+//       <AbandonButton>Leave</AbandonButton> : 
+//       <AttendButton>Join</AttendButton>
+//   }
 }
 
+
+
+//EventButton = connect()(EventButton);
 export default EventButton;
