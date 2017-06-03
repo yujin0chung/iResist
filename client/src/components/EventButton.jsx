379320f895fb1b0118a1fce4b9f5747c9893ee44
yujin0chung <yujin0chung@gmail.com>
2017-06-03 15:23:16 -0700

    Adds EditButton and adds logic so leaders will see Make Changes buttton

--- client/src/components/EventButton.jsx
+++ client/src/components/EventButton.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { AttendButton, AbandonButton } from './StyledComponents.jsx';
+import { AttendButton, AbandonButton, EditButton } from './StyledComponents.jsx';
 
 const EventButton = (props) => {
 
-  const participating = props.role === 'organizing' || props.role === 'attending';
+  const leader = props.leader;
+  if (leader) { return (<EditButton>Make Changes</EditButton>) }
 
+  const participating = props.role === 'organizing' || props.role === 'attending';
   return participating ? 
-    (<AbandonButton onClick={() => props.removeUserFromEvent(props.protestId, props.userId)}>Betray the cause</AbandonButton>) :  
+    (<AbandonButton onClick={() => {
+       props.removeUserFromEvent(props.protestId, props.userId);
+       props.changeView('DASHBOARD');
+     }}>Betray the cause</AbandonButton>) :  
     (<AttendButton onClick={() => props.addUserToEvent(props.protestId, props.userId)}>Join</AttendButton>);
 }
 
