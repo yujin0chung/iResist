9e68d4eb5f6d7836f8ca4df246d7517ed39a2a8d
yujin0chung <yujin0chung@gmail.com>
2017-06-03 13:44:46 -0700

    Fixes merge conflicts

--- client/src/components/ProtestForm.jsx
+++ client/src/components/ProtestForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { HomeIcon } from './HomeIcon.jsx';
+import { HomeIcon } from './StyledComponents.jsx';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import axios from 'axios';
 
