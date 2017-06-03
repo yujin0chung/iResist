8bf0bf14a7deade6b8ceec820773c5242962009f
yujin0chung <yujin0chung@gmail.com>
2017-06-03 10:52:47 -0700

    Writes out case for join event and join error

--- client/src/reducers/eventReducer.js
+++ client/src/reducers/eventReducer.js
@@ -10,5 +10,23 @@ export const events = (state = {}, action) => {
       })
     default:
       return state;
-  }
-};
+
+// export const eventsReducer = (state = {}, action) => {
+//   switch(action.type) {
+//     case 'RECEIVE_EVENT':
+//       return Object.assign({}, state, {
+//         event: action.data
+//       })
+//     case 'CREATE_ERROR':
+//       return Object.assign({}, state, {
+//         error: action.data
+//       })
+//     default:
+//       return state;
+//   }
+// };
+
+
+
+
+
