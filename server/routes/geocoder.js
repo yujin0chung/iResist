d3326d8d050c4761436c15393ec6e747adb45c6f
Alex Seiden <alex.seiden@gmail.com>
2017-06-03 18:02:21 -0700

    (feat) add api auth middleware

--- server/routes/geocoder.js
+++ server/routes/geocoder.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const middleware = require('../middleware');
 const controllers = require('../controllers').Geocoder;
 
-router.get('/', controllers.geocode);
+router.get('/', middleware.auth.apiVerify, (req, res) => {
+  controllers.geocode(req, res);
+});
 
 module.exports = router;
