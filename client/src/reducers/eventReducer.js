c046071a8c9fa7b81598d8a5f1d2ffe3f89468ce
Alex Seiden <alex.seiden@gmail.com>
2017-06-05 16:39:15 -0700

    (feat) reduce get al events

--- client/src/reducers/eventReducer.js
+++ client/src/reducers/eventReducer.js
@@ -1,33 +1,37 @@
 export const events = (state = {}, action) => {
   switch (action.type) {
-    case 'RECEIVE_EVENT':
-      return Object.assign({}, state, {
-        event: action.data
-      })
-    case 'CREATE_ERROR':
-      return Object.assign({}, state, {
-        error: action.data
-      })
-    case 'JOIN_EVENT':
-      return Object.assign({}, state, {
-        eventId: action.eventId,
-        userId: action.userId
-      })
-    case 'JOIN_ERROR':
-      return Object.assign({}, state, {
-        error: action.error
-      })
-    case 'LEAVE_EVENT': 
-      return Object.assign({}, state, {
-        eventId: action.eventId,
-        userId: action.userId
-      })
-    case 'LEAVE_ERROR':
-      return Object.assign({}, state, {
-        error: action.error
-      })
-    default:
-      return state;
+  case 'RECEIVE_EVENT':
+    return Object.assign({}, state, {
+      event: action.data
+    });
+  case 'CREATE_ERROR':
+    return Object.assign({}, state, {
+      error: action.data
+    });
+  case 'JOIN_EVENT':
+    return Object.assign({}, state, {
+      eventId: action.eventId,
+      userId: action.userId
+    });
+  case 'JOIN_ERROR':
+    return Object.assign({}, state, {
+      error: action.error
+    });
+  case 'LEAVE_EVENT':
+    return Object.assign({}, state, {
+      eventId: action.eventId,
+      userId: action.userId
+    });
+  case 'LEAVE_ERROR':
+    return Object.assign({}, state, {
+      error: action.error
+    });
+  case 'GET_ALL_EVENTS_SUCCESS':
+    return Object.assign({}, state, {
+      events: action.events
+    });
+  default:
+    return state;
   }
 };
 
