6568ffaa83e9a773dc06e3f7c355165803e12992
yujin0chung <yujin0chung@gmail.com>
2017-06-05 09:05:34 -0700

    Betray Cause button changes to Join Button upon click for MVP purposes

--- client/src/components/EventButton.jsx
+++ client/src/components/EventButton.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import _ from 'lodash';
 import { AttendButton, AbandonButton, EditButton } from './StyledComponents.jsx';
 
-const EventButton = (props) => {
+/*const EventButton = (props) => {
   console.log('EVENT BUTTON PROPS', props)
   console.log('EVENT BUTTON USER ID', props.userId)
   const leader = props.leader;
@@ -19,6 +19,39 @@ const EventButton = (props) => {
        {props.addUserToEvent(props.protestId, props.userId);
        props.changeView('DASHBOARD')}}>Join
      </AttendButton>);
-}
+}*/
 
+class EventButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({clicked: !this.state.clicked});
+  };
+
+  render() {
+    const leader = this.props.leader;
+    if (leader) { return (<EditButton>Make Changes</EditButton>) }
+    const participating = this.props.role === 'organizing' || this.props.role === 'attending';
+    return this.state.clicked ? 
+      (<AbandonButton onClick={() => {
+        this.props.removeUserFromEvent(this.props.protestId, this.props.userId);
+        this.handleClick();
+        this.props.changeView('DASHBOARD');
+      }}>Betray the cause</AbandonButton>) :  
+      (<AttendButton onClick={() => 
+        {this.handleClick(); this.props.addUserToEvent(this.props.protestId, this.props.userId);        
+        this.props.changeView('DASHBOARD') }}>Join
+      </AttendButton>);
+      
+
+  }
+} 
+
+  
 export default EventButton;
