8dc3807fd207839116b27e2f4bb4d342c45bdab1
yujin0chung <yujin0chung@gmail.com>
2017-06-08 13:12:43 -0700

    Removes Make Changes button if event is passed

--- client/src/components/EventButton.jsx
+++ client/src/components/EventButton.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import _ from 'lodash';
-import { AttendButton, AbandonButton, EditButton } from './StyledComponents.jsx';
+import React from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
+import {
+  AttendButton,
+  AbandonButton,
+  EditButton
+} from "./StyledComponents.jsx";
 
-const EventButton = (props) => {
-
+const EventButton = props => {
   const leader = props.leader;
-  if (leader) { return (<EditButton onClick={() => props.changeView('ORGANIZE_PROTEST', props.protestId)}>Make Changes</EditButton>) }
+  const event = _.find(props.events.allEvents, event => event.id === props.protestId);
+  if (leader && event.status !== "passed") {
+    return (
+      <EditButton onClick={() => props.changeView("ORGANIZE_PROTEST", props.protestId)}>
+        Make Changes
+      </EditButton>
+    );
+  } else {
+    return <div />;
+  }
 
-  const participating = props.role === 'organizing' || props.role === 'attending';
-  return participating ? 
-    (<AbandonButton onClick={() => {
-       props.removeUserFromEvent(props.protestId, props.userId);
-       props.changeView('DASHBOARD');
-    }}>Betray the cause</AbandonButton>) :  
-    (<AttendButton onClick={() => 
-       {props.addUserToEvent(props.protestId, props.userId);
-       props.changeView('DASHBOARD')}}>Join
-     </AttendButton>);
-}
-
+  const participating = props.role === "organizing" || props.role === "attending";
+  return participating? 
+    <AbandonButton onClick={() => {
+      props.removeUserFromEvent(props.protestId, props.userId);
+      props.changeView("DASHBOARD");
+      }}
+      >Betray the cause
+    </AbandonButton> : 
+    <AttendButton onClick={() => {
+      props.addUserToEvent(props.protestId, props.userId);
+      props.changeView("DASHBOARD");
+      }}
+      >Join
+    </AttendButton>;
+};
 
 export default EventButton;
