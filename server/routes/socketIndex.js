a91a1ae1a7a2bc65e5ae5f24f3d19445d8011fe0
Alex Seiden <alex.seiden@gmail.com>
2017-06-08 11:29:37 -0700

    (feat) stub out socket.io listener router

---
+++ server/routes/socketIndex.js
@@ -0,0 +1,13 @@
+const socket = require('../index');
+const io = socket.io;
+
+io.on('connection', function(client) {
+  client.on('room', function(event) {
+    client.join(event);
+    io.to(event).emit('roomResponse', 'You are in room: ' + room);
+
+    // client.on('comment', function(post) {
+    //   socketController.comments.addComment(post, room, client);
+    // });
+  });
+});
