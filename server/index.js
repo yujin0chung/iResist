0d4229abbedc09c1e6d60e54c728ee4f50f7c771
Alex Seiden <alex.seiden@gmail.com>
2017-06-08 12:52:12 -0700

    (feat) implement socket listener and listerner router

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@
 const app = require('./app');
 const db = require('../db');
 const PORT = process.env.PORT || 3000;
+const socketRoutes = require('./routes/socketIndex');
+const socketAuth = require('./middleware/socketAuth');
 
 const server = app.listen(PORT, () => {
   console.log('Example app listening on port: ', PORT);
 });
 
 const io = require('socket.io')(server);
+
+io.use(socketAuth);
+
+io.on('connection', function(client) {
+  socketRoutes.onConnect(client, io);
+});
 
 module.exports.io = io;
