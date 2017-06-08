37e7f7cc44f98408c63aefa179e32838b52dfc7d
Alex Seiden <alex.seiden@gmail.com>
2017-06-08 15:16:01 -0700

    (feat) stub out day of container

---
+++ client/src/components/DayOfContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class DayOContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.store = {};
+  }
+
+  render () {
+    return (
+      <h1>You are looking at the day of container</h1>
+    );
+  }
+}
+
+export default DayOfContainer;
