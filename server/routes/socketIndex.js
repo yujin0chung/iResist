d1babebf38efa9c06212c8e8f967bd0a561e5aba
Alex Seiden <alex.seiden@gmail.com>
2017-06-08 12:18:15 -0700

    (feat) fix typo

--- server/routes/socketIndex.js
+++ server/routes/socketIndex.js
@@ -4,7 +4,7 @@ const io = socket.io;
 io.on('connection', function(client) {
   client.on('room', function(event) {
     client.join(event);
-    io.to(event).emit('roomResponse', 'You are in room: ' + room);
+    io.to(event).emit('roomResponse', 'You are in room: ' + event);
 
     // client.on('comment', function(post) {
     //   socketController.comments.addComment(post, room, client);
