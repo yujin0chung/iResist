c69268e9d5a5df1336920a77c90f1c09ab5c027f
yujin0chung <yujin0chung@gmail.com>
2017-06-08 17:02:30 -0700

    Adds cases to conditional rendering of button type

--- client/src/components/EventButton.jsx
+++ client/src/components/EventButton.jsx
@@ -1,27 +1,55 @@
 import React from "react";
 import { connect } from "react-redux";
 import _ from "lodash";
-import {
-  AttendButton,
-  AbandonButton,
-  EditButton
-} from "./StyledComponents.jsx";
+import { AttendButton, AbandonButton, EditButton } from "./StyledComponents.jsx";
 
 const EventButton = props => {
+
   const leader = props.leader;
   const event = _.find(props.events.allEvents, event => event.id === props.protestId);
+  const participating = props.role === "organizing" || props.role === "attending";
+  const attending = props.role === "attending";
+
+  let button;
+
+  if (leader && event.status === "upcoming") {
+    button = <EditButton onClick={() => props.changeView("ORGANIZE_PROTEST", props.protestId)}>Make Changes</EditButton>
+  } 
+  else if (attending && event.status !== "passed") {
+    button = <AbandonButton onClick={() => {
+      props.removeUserFromEvent(props.protestId, props.userId);
+      props.changeView("DASHBOARD");
+      }}
+      >Betray the cause
+    </AbandonButton> 
+  } 
+  else if (!participating && !leader && event.status !== "passed") {
+    button =   <AttendButton onClick={() => {
+      props.addUserToEvent(props.protestId, props.userId);
+      props.changeView("DASHBOARD");
+      }}
+      >Join
+    </AttendButton>
+  } 
+  else { 
+    button = <div></div>
+  }
+  {console.log('EVENT BUTTON', button)}
+
+       return ( <div>{button}</div> );
+            
+
+};
+
+/*
   if (leader && event.status !== "passed") {
     return (
       <EditButton onClick={() => props.changeView("ORGANIZE_PROTEST", props.protestId)}>
         Make Changes
       </EditButton>
     );
-  } else {
-    return <div />;
-  }
-
-  const participating = props.role === "organizing" || props.role === "attending";
-  return participating? 
+  } 
+  return participating && event.status !== "passed" ? 
     <AbandonButton onClick={() => {
       props.removeUserFromEvent(props.protestId, props.userId);
       props.changeView("DASHBOARD");
@@ -33,7 +61,7 @@ const EventButton = props => {
       props.changeView("DASHBOARD");
       }}
       >Join
-    </AttendButton>;
-};
+    </AttendButton>;*/
+
 
 export default EventButton;
