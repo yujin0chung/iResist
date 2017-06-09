0675aeeff0c8688675e1d90386356ba6340ae63f
yujin0chung <yujin0chung@gmail.com>
2017-06-09 16:45:18 -0700

    Adds action creators for posting message to feed

--- client/src/actions/feedActions.js
+++ client/src/actions/feedActions.js
@@ -18,3 +18,28 @@ export const deleteItem = () => {
     // INSERT OTHER CHANGES HERE
   };
 };
+
+export const postMessageSuccess = message => {
+  return {
+    type: 'POST_MESSAGE_SUCCESS',
+    message
+  };
+};
+
+export const postMessageError = error => {
+  return {
+    type: 'POST_MESSAGE_ERROR',
+    error
+  };
+};
+
+
+export const postMessage = message => dispatch => {
+  axios.post('/api/feed/postMessage', message)
+    .then(response => {
+      dispatch(postMessageSuccess(message));
+    })
+    .catch(error => {
+      dispatch(postMessageError(error));
+    });
+};
