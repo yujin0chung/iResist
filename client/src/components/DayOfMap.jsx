6e682c40e0ab4cdf5404e4298cbfca3e5a2b2f3b
Alex Seiden <alex.seiden@gmail.com>
2017-06-10 11:42:31 -0700

    (feat) stub out map

--- client/src/components/DayOfMap.jsx
+++ client/src/components/DayOfMap.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 class DayOfMap extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h2>This will be the map</h2>
+    );
+  }
 }
 
 export default DayOfMap;
