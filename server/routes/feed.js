4223df63914e29be2ab75d49155dd785d6975a2d
yujin0chung <yujin0chung@gmail.com>
2017-06-10 15:15:05 -0700

    Sends differently formatted feed data to client

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controllers = require('../controllers').Feed;
 
 router.get('/all', controllers.getAllFeed);
+router.get('/event', controllers.getEventFeed);
 
 module.exports = router;
