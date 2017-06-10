6013331736ed4929ce44a6c29aecd68fcd399a43
yujin0chung <yujin0chung@gmail.com>
2017-06-09 17:09:54 -0700

    Adds get feed messages action creators

--- client/src/actions/feedActions.js
+++ client/src/actions/feedActions.js
@@ -19,6 +19,34 @@ export const deleteItem = () => {
   };
 };
 
+export const getFeedMessagesSuccess = messages => {
+  return {
+    type: 'GET_MESSAGES_SUCCESS',
+    messages
+  };
+};
+
+export const getFeedMessagesError = error => {
+  return {
+    type: 'GET_MESSAGES_ERROR',
+    error
+  };
+};
+
+export const getFeedMessages = (eventId) => dispatch => {
+  axios.get('/api/feed/messages', {
+    params: {
+      eventId: eventId
+    }
+  })
+  .then(response => {
+    dispatch(getFeedMessagesSuccess(response.data));
+  })
+  .catch(error => {
+    dispatch(getFeedMessagesError(error));
+  });
+};
+
 export const postMessageSuccess = message => {
   return {
     type: 'POST_MESSAGE_SUCCESS',
