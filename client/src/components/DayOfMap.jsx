c93f55e76b47a59bda4f2e4bfca746c856cedf14
Alex Seiden <alex.seiden@gmail.com>
2017-06-10 12:26:29 -0700

    (feat) center day-of map on protest start and drop marker pin

--- client/src/components/DayOfMap.jsx
+++ client/src/components/DayOfMap.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 class DayOfMap extends React.Component {
   constructor(props) {
@@ -8,9 +9,22 @@ class DayOfMap extends React.Component {
   }
 
   render() {
+    const mapId = this.props.events.allEvents[this.props.events.activeEvent].mapId;
+    const currentMap = this.props.maps.allMaps[mapId];
     return (
       <div>
         <h2>This will be the map</h2>
+        <Map className='day-of-map' center={[currentMap.lat, currentMap.long]} zoom={15}>
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[currentMap.lat, currentMap.long]}>
+            <Popup>
+              <span>Event meeting location</span>
+            </Popup>
+          </Marker>
+        </Map>
       </div>
     );
   }
