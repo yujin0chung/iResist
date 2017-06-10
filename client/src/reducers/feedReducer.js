132c920e742fcba092096f24aa527439083fb9c1
yujin0chung <yujin0chung@gmail.com>
2017-06-10 15:16:31 -0700

    Adds types for feed action creators and adds feeds reducer to index.js

--- client/src/reducers/feedReducer.js
+++ client/src/reducers/feedReducer.js
@@ -1,13 +1,22 @@
-export const feed = (state = {}, action) => {
+export const feeds = (state = {}, action) => {
   switch (action.type) {
-  case 'GET_ALL_FEED_SUCCESS':
+  case 'GET_FEEDS_SUCCESS':
     return Object.assign({}, state, {
-      feed: action.feed
+      feeds: action.feeds.data.feeds
     });
-  case 'GET_ALL_FEED_ERROR':
+  case 'GET_FEED_ITEMS_SUCCESS':
+    return Object.assign({}, state, {
+      feedItems: action.feedItems.data.feedItems
+    })
+  case 'GET_FEEDS_ERROR':
     return Object.assign({}, state, {
       error: action.error
     });
+  case 'GET_FEED_ITEMS_ERROR':
+    return Object.assign({}, state, {
+      error: action.error
+    })
   default: return state;
   }
 };
+
