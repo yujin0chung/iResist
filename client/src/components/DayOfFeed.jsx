981c1e21fb78213e68e3a2db8f433e8db721fe4a
yujin0chung <yujin0chung@gmail.com>
2017-06-12 13:53:20 -0700

    removes console logs

--- client/src/components/DayOfFeed.jsx
+++ client/src/components/DayOfFeed.jsx
@@ -14,42 +14,32 @@ class DayOfFeed extends React.Component {
       username: this.props.user.username,
       feedId: '' || 1,
       type: '' || 'MESSAGE',
-      posts: [],
       feedItemId: ''
     };
-  this.handlePost = this.handlePost.bind(this);
-  this.handleChange = this.handleChange.bind(this);
+    this.handlePost = this.handlePost.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    const { client, feedItems } = this.props;
-   //this.props.getFeeds(this.props.events.activeEvent);
-
+    const { client } = this.props;
     client.on('postedFeedItemId', (id) => {
       this.setState({feedItemId: id[0]});
-    })
-
-    client.on('newFeedItemFromServer', post => {
-      this.props.receiveFeedItem(post);
-    })
+    });
   }
-
 
   handlePost(e) {
     e.preventDefault();
     let newPost = {
-      text: this.state.text,
+      itemId: this.state.feedItemId,
+      eventId: this.props.events.activeEvent,
       type: this.state.type,
+      text: this.state.text,
       url: this.state.url,
-      credibility: this.state.credibility,
       userId: this.state.userId,
       username: this.state.username,
-      feedId: this.state.feedId,
-      feedItemId: this.state.feedItemId
+      credibility: this.state.credibility
     };
     this.props.client.emit('newFeedItem', newPost);
-    this.state.posts.push(newPost);
-    // this.props.postItem(newPost);
     this.setState({
       text: '',
       url: '',
@@ -69,17 +59,18 @@ class DayOfFeed extends React.Component {
       <div>
         <h3>Post a message</h3>
         <form onSubmit={this.handlePost}>
-        <input
-          type="textarea"
-          placeholder="What's happening at the protest?"
-          value={this.state.text}
-          onChange={this.handleChange}
-        />
+          <input
+            type="textarea"
+            placeholder="What's happening at the protest?"
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
         </form>
         {(feedItems !== undefined && !Object.is(feedItems, {})) ? 
           feedItems.map(item => <div><FeedItem username={item.username} text={item.text} key={this.state.feedItemId}/></div>) :
           <div></div>
         }
+
       </div>
     );
   }
--- server/routes/socketIndex.js
+++ server/routes/socketIndex.js
@@ -8,35 +8,26 @@ const formatFeed = require('../../db/lib/formatFeed');
 module.exports.onConnect = (client, io) => {
 
   client.on('event', function(event) {    
-
     client.join(event);
-
-    models.Feed.getFeedByEventId(event, (err, feedItems) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const feeds = formatFeed(feedItems);
-        const feed_items = formatFeedItems(feedItems);
-        io.emit('fetchFeedItems', {feeds, feed_items})
-      }
-    });
-    // io.to(event).emit('joinEventReponse', 'You are in event: ' + event);
     client.emit('joinEventResponse', 'You are in event: ' + event);
   });
 
   client.on('disconnect', () => {
-    console.log('user disconnected')
+    console.log('user disconnected');
   });
 
   client.on('newFeedItem', (post) => {
-      models.Feed.postItem(post, (err, id) => {
-        if (err) {
-          console.error(err);
-        } else {
-          client.emit('postedFeedItemId', id);
-        }
-      });
-    io.emit('newFeedItemFromServer', post);
+    models.Feed.postItem(post, (err, insertedPost) => {
+
+      if (err) {
+        console.error(err);
+      } else {
+
+        client.emit('postedFeedItemId', insertedPost[0].id);
+        client.emit('newFeedItemFromServer', insertedPost[0]);
+      }
+    });
+
   });
 };
 
