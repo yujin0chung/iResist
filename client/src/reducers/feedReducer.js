41a1af00f845833c9b5754c0b5ef3c6a9b320b7a
yujin0chung <yujin0chung@gmail.com>
2017-06-12 12:10:28 -0700

    Removes getallfeed and postitem cases from feeds reducer

--- client/src/reducers/feedReducer.js
+++ client/src/reducers/feedReducer.js
@@ -1,35 +1,18 @@
 export const feeds = (state = {}, action) => {
   switch (action.type) {
   case 'RECEIVE_FEED_ITEM': 
-    console.log('ACTION FROM RECEIVE FEED', action)
-    console.log('STATE FROM RECEIVE FEED ITEM', state)
-    // let newState = Object.assign({}, state);
-    // let feedItemId = action.item.feedItemId;
-    // if (action.item.length > 0) {
-    //   newState.feedItems[feedItemId] = action.item;
-    // }
-    // console.log('NEW STATE', newState)
-    // newState.feedItems[action.item.feedItemId] = action.item;
+    console.log('STATE FROM RECEIVE FEED ITEM', state);
     return Object.assign({}, state, {
-      //feedItems: [...state.feedItems, action.item]
       feedItems: [...state.feedItems, action.item]
   });
-  case 'GET_FEEDS_SUCCESS':
-    return Object.assign({}, state, {
-      feeds: action.feeds.data.feeds
-    });
   case 'GET_FEED_ITEMS_SUCCESS':
     return Object.assign({}, state, {
       feedItems: action.feedItems.data.feedItems
     });
-  case 'GET_FEEDS_ERROR':
-    return Object.assign({}, state, {
-      error: action.error
-    });
   case 'GET_FEED_ITEMS_ERROR':
     return Object.assign({}, state, {
       error: action.error
-    })
+    });
   case 'POST_FEED_ITEM_SUCCESS':
     return Object.assign({}, state, {
       item: action.item
