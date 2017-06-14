762ff1899e6e53c560b5ec358fee00d8131de313
Russ Ward <maddogward@gmail.com>
2017-06-14 10:24:19 -0700

    Resize images and Rotate component

--- client/src/components/StyledComponents.jsx
+++ client/src/components/StyledComponents.jsx
@@ -75,8 +75,8 @@ export const Icon = styled.img`
 
 export const Rotate = styled.div`
   transform: rotate(90deg);
-  height: 350px;
-  margin: 10px, 50px, 10px, 0px;
+  height: 360px;
+  margin-padding: 10px, 50px, 10px, 0px;
  `;
 
 export const Tab = styled.div`
