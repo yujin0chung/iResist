2c0172ce8c91b302bc27643da0804601111f8868
tabithahsia <tabithahsia@gmail.com>
2017-06-14 14:18:28 -0700

    Testing to see if it works on heroku - will refactor after if it works

--- server/app.js
+++ server/app.js
@@ -10,11 +10,45 @@ const creds = require('../config/configVars');
 
 const app = express();
 
+try {
+  creds = require('../../config/configVars');
+} catch (e) {
+}
+
+let twitterKey;
+let twitterSecret;
+let twitterAccessToken;
+let twitterAccessTokenSecret;
+
+if (process.env.TWITTER_ACCESS_TOKEN) {
+  twitterKey = TWITTER_ACCESS_TOKEN
+} else {
+  twitterKey = creds.twitterKey
+}
+
+if (process.env.TWITTER_ACCESS_TOKEN_SECRET) {
+  twitterSecret = TWITTER_ACCESS_TOKEN_SECRET
+} else {
+  twitterSecret = creds.twitterSecrets;
+}
+
+if (process.env.TWITTER_KEY) {
+  twitterAccessToken = TWITTER_KEY;
+} else {
+  twitterAccessToken = creds.access_token;
+}
+
+if(process.env.TWITTER_SECRET) {
+  twitterAccessTokenSecret = TWITTER_SECRET;
+} else {
+  twitterAccessTokenSecret = creds.twitterAccessTokenSecret
+}
+
 var T = new Twit({
-    consumer_key: creds.twitterKey,
-    consumer_secret: creds.twitterSecret,
-    access_token: creds.twitterAccessToken,
-    access_token_secret: creds.twitterAccessTokenSecret,
+    consumer_key: twitterKey,
+    consumer_secret: twitterSecret,
+    access_token: twitterAccessToken,
+    access_token_secret: twitterAccessTokenSecret,
     timeout_ms: 60*1000
 });
 
