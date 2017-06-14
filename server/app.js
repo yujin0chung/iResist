1a9e3ef7f041f057d11b2c4532463469109918d2
tabithahsia <tabithahsia@gmail.com>
2017-06-14 14:55:36 -0700

    Typo

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ let twitterAccessToken;
 let twitterAccessTokenSecret;
 
 if (process.env.TWITTER_KEY) {
-  twitterKey = process.env.KEY;
+  twitterKey = process.env.TWITTER_KEY;
 } else {
   twitterKey = creds.twitterKey;
 }
