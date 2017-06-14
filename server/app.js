d82157eb9e288702b4859fa19d5ae093b85a71cb
tabithahsia <tabithahsia@gmail.com>
2017-06-14 14:47:16 -0700

    Mixed up keys

--- server/app.js
+++ server/app.js
@@ -20,28 +20,28 @@ let twitterSecret;
 let twitterAccessToken;
 let twitterAccessTokenSecret;
 
-if (process.env.TWITTER_ACCESS_TOKEN) {
-  twitterKey = process.env.TWITTER_ACCESS_TOKEN
+if (process.env.TWITTER_KEY) {
+  twitterKey = process.env.TWITTER_ACCESS_TOKEN;
 } else {
-  twitterKey = creds.twitterKey
+  twitterKey = creds.twitterKey;
 }
 
-if (process.env.TWITTER_ACCESS_TOKEN_SECRET) {
-  twitterSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET
+if (process.env.TWITTER_SECRET) {
+  twitterSecret = process.env.TWITTER_SECRET;
 } else {
   twitterSecret = creds.twitterSecret;
 }
 
-if (process.env.TWITTER_KEY) {
-  twitterAccessToken = process.env.TWITTER_KEY;
+if (process.env.TWITTER_ACCESS_TOKEN) {
+  twitterAccessToken = process.env.TWITTER_ACCESS_TOKEN;
 } else {
   twitterAccessToken = creds.twitterAccessToken;
 }
 
-if(process.env.TWITTER_SECRET) {
-  twitterAccessTokenSecret = process.env.TWITTER_SECRET;
+if(process.env.TWITTER_ACCESS_TOKEN_SECRET) {
+  twitterAccessTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
 } else {
-  twitterAccessTokenSecret = creds.twitterAccessTokenSecret
+  twitterAccessTokenSecret = creds.twitterAccessTokenSecret;
 }
 
 var T = new Twit({
