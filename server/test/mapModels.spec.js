3a1407fffa581f152fbd58a16ffdd70afc55729e
Alex Seiden <alex.seiden@gmail.com>
2017-06-14 15:18:47 -0700

    (feat) test map upload

---
+++ server/test/mapModels.spec.js
@@ -0,0 +1,46 @@
+const expect = require('chai').expect;
+const map = require('../../db/models/map');
+const event = require('../../db/models/events');
+const user = require('../../db/models/user');
+const dbUtils = require('../../db/lib/utils.js');
+
+describe('Map model tests', function() {
+    // Deletes all tables, creates new tables, and seeds tables with test data
+  beforeEach(function (done) {
+    dbUtils.rollbackMigrate(function() {
+      user.insertUser('test-user', function() {
+        event.createEvent({ eventId: '',
+          name: 'test event',
+          userId: 1,
+          description: 'okie dokie',
+          cause: 'time to go!',
+          address: 'san francisco',
+          date: '2017-10-10',
+          timeStart: '10:00',
+          timeEnd: '17:10',
+          lat: 37.7792808,
+          long: -122.4192363,
+          protests: [ [ 37.7792808, -122.4192363 ] ],
+          zoom: 14,
+          timezone: '',
+          isEditing: false },
+        function(err, data) {
+          done();
+        });
+      });
+    });
+  });
+
+  // Resets database back to original settings
+  afterEach(function (done) {
+    dbUtils.rollback(done);
+  });
+
+  it('should be able to make new maps', function(done) {
+    map.makeMap(1, 0, 0, function(err, data) {
+      expect(err).to.equal(null);
+      expect(data.rowCount).to.equal(1);
+      done();
+    });
+  });
+});
