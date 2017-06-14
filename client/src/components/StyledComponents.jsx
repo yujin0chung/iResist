3fd84d987adfda4722ba2da5814b2c9b8e10e5ac
Russ Ward <maddogward@gmail.com>
2017-06-14 08:45:38 -0700

    Add media container class to css

--- client/src/components/StyledComponents.jsx
+++ client/src/components/StyledComponents.jsx
@@ -76,6 +76,7 @@ export const Icon = styled.img`
 export const Rotate = styled.div`
   transform: rotate(90deg);
   height: 350px;
+  margin: 10px, 50px, 10px, 0px;
  `;
 
 export const Tab = styled.div`
