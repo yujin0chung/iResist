f2f13123163ca7ae55ebadbd6c8037d41370b8fc
yujin0chung <yujin0chung@gmail.com>
2017-06-15 12:34:19 -0700

    adjusts feed items spacing

--- client/src/components/FeedItem.jsx
+++ client/src/components/FeedItem.jsx
@@ -90,16 +90,16 @@ class FeedItem extends React.Component {
     var time = moment(Number(this.props.time)).format("h:mm a");
     return (
       <Item>
+  
         <VoteHolder>
           <VoteUp className="fa fa-caret-up fa-lg" onClick={() => this.handleVoteType(1)} style={{width: '50%'}} src='images/UpArrow.svg'/>
           <div style={{textAlign: 'inherit',color:'#9B9B9B'}}>{this.props.credibility}</div>
           <VoteDown className="fa fa-caret-down fa-lg" onClick={() => this.handleVoteType(-1)} style={{width: '50%'}}  src='images/DownArrow.svg'/>
         </VoteHolder>
-        <div style={{ flexDirection: "row", flex: 8, paddingTop: '11px', paddingLeft: '10px' }}>
+        <div style={{ flexDirection: "row", flex: 8, paddingLeft: '10px' }}>
           <div style={{color:'#4A4A4A'}}>{this.props.username}</div>
           <div style={{color:'#C5C5C5', fontSize:'.8em'}}><img style={{width:'1em', marginRight:'5px'}} src='images/Clock.svg'/>{time}</div>
           <div>{this.state.element}</div>
-          {/*<div className="credibility" />*/}
         </div>
       </Item>
     );
@@ -111,15 +111,12 @@ const Item = styled.div`
   flex-direction: row;
   align-items: baseline;
   border: 1px solid #eee;
-  margin-bottom: 13px;
-  margin-top: 13px;
+  margin-top: 9px;
   border-right: none;
   border-left: none;
-  border-bottom: none;
-  &:first-child {
-    border-top: none;
-  }
+  border-top: none;
 `;
+
 const VoteHolder = styled.div`
   flex:1;
   width: 3em;
