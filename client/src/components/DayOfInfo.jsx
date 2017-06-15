37395f036cf049c0e58ce18168f0d8a81a6f8603
yujin0chung <yujin0chung@gmail.com>
2017-06-15 02:09:57 -0700

    adds styling and navbar icon images

--- client/src/components/DayOfInfo.jsx
+++ client/src/components/DayOfInfo.jsx
@@ -1,12 +1,53 @@
 import React from 'react';
+import styled from 'styled-components'
 
 const DayOfInfo = (props) => (
   <div>
-    <h2>Ongoing Protest: {props.event.name}</h2>
-    <h3>Cause: {props.event.cause}</h3>
-    <h3>Description: {props.event.description}</h3>
-    <h3>Participating: {props.event.attendee_count}</h3>
+    <PageTitle>{props.event.name}</PageTitle>
+      <ProtestInfo>
+        <div>
+          <Category>What</Category>
+           <div>{props.event.cause}</div>
+        </div>
+        <div>
+          <Category>Where</Category>
+           <div>{props.event.address}</div>
+        </div>
+        <div>
+          <Category>When</Category>
+           <div>{props.event.time}</div>
+        </div>
+        <div>
+          <Category>Why</Category>
+           <div>{props.event.description}</div>
+        </div>
+        <div>
+          <Category>Participating</Category>
+           <div>{props.event.attendee_count}</div>
+        </div>                
+      </ProtestInfo>
   </div>
 );
+
+const Category = styled.div`
+  font-family: 'Amiko', sans-serif;
+  font-weight:bold;
+  font-size: 12.5px;
+`
+
+const PageTitle = styled.div`
+  font-family: 'Cousine', monospace;
+  font-size: 25px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+      text-align: center;
+`;
+
+export const ProtestInfo = styled.div`
+
+  margin-left: 60px;
+  margin-top: 45px;
+`
+
 
 export default DayOfInfo;
--- client/src/components/FeedItem.jsx
+++ client/src/components/FeedItem.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import moment from 'moment';
-import { Rotate } from './StyledComponents.jsx'
+import React from "react";
+import moment from "moment";
+import { Rotate } from "./StyledComponents.jsx";
+import styled from "styled-components";
+
 
 class FeedItem extends React.Component {
   constructor(props) {
@@ -8,42 +10,74 @@ class FeedItem extends React.Component {
     this.state = {
       userId: this.props.userId,
       itemId: this.props.itemId,
-      element: ''
+      element: ""
     };
     this.handleVoteType = this.handleVoteType.bind(this);
   }
 
-
   componentWillMount() {
-    if (this.props.type === 'image/jpeg') {
-      this.state.element =
-        <div className='mediaContainer' >
-          <img className='media' id={this.props.itemId} onDoubleClick={this.rotate.bind(this)} src={this.props.url} />
+    if (this.props.type === "image/jpeg") {
+      this.state.element = (
+        <div className="mediaContainer">
+          <img
+            style={{ flex: 1,width: '75%',height: '75%'}}
+            className="media"
+            id={this.props.itemId}
+            onDoubleClick={this.rotate.bind(this)}
+            src={this.props.url}
+          />
         </div>
-    } else if (this.props.type === 'image/png') {
-      this.state.element =
-        <div className='mediaContainer'>
-          <img className='media' id={this.props.itemId} onDoubleClick={this.rotate.bind(this)} src={this.props.url} />
+      );
+    } else if (this.props.type === "image/png") {
+      this.state.element = (
+        <div className="mediaContainer">
+          <img
+            className="media"
+            id={this.props.itemId}
+            onDoubleClick={this.rotate.bind(this)}
+            src={this.props.url}
+          />
         </div>
-    } else if (this.props.type === 'video/mp4') {
-      this.state.element =
-        <div className='mediaContainer'>
-          <video className='media' className='media' id={this.props.itemId} src={this.props.url} autoPlay={false} type='video/mp4' controls/>
+      );
+    } else if (this.props.type === "video/mp4") {
+      this.state.element = (
+        <div className="mediaContainer">
+          <video
+            className="media"
+            className="media"
+            id={this.props.itemId}
+            src={this.props.url}
+            autoPlay={false}
+            type="video/mp4"
+            controls
+          />
         </div>
-    } else if (this.props.type === 'video/quicktime') {
-      this.state.element =
-        <div className='mediaContainer'>
-          <video className='media' className='media' id={this.props.itemId} src={this.props.url} autoPlay={false} type='video/quicktime' controls/>
+      );
+    } else if (this.props.type === "video/quicktime") {
+      this.state.element = (
+        <div className="mediaContainer">
+          <video
+            className="media"
+            className="media"
+            id={this.props.itemId}
+            src={this.props.url}
+            autoPlay={false}
+            type="video/quicktime"
+            controls
+          />
         </div>
-    }  else {
-      this.state.element = <span id={this.props.itemId}>{this.props.text}</span>
+      );
+    } else {
+      this.state.element = (
+        <span id={this.props.itemId}>{this.props.text}</span>
+      );
     }
   }
 
   rotate() {
     this.setState({
       element: <Rotate>{this.state.element}</Rotate>
-    })
+    });
   }
 
   handleVoteType(polarity) {
@@ -55,20 +89,28 @@ class FeedItem extends React.Component {
   }
 
   render() {
-    var time = moment(Number(this.props.time)).format('h:mm a');
-    console.log('this is the current this.props.time: ', this.props.time);
-    console.log('this is the current time: ', this.state.time);
+    var time = moment(Number(this.props.time)).format("h:mm a");
     return (
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-        <span> <b>{this.props.username}</b> : {this.state.element} at {time} </span>
-        <div className="credibility">
-          <input placeholder="How credible?" value={this.props.credibility} />
-          <button onClick={() => this.handleVoteType(1)}>Upcred</button>
-          <button onClick={() => this.handleVoteType(-1)}>Downcred</button>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <VoteHolder>
+          <button onClick={() => this.handleVoteType(1)}><img style={{width: '60%'}} src='images/UpArrow.svg'/></button>
+          <div style={{textAlign: 'center',color:'#9B9B9B'}}>{this.props.credibility}</div>
+          <button onClick={() => this.handleVoteType(-1)}><img  style={{width: '60%'}}  src='images/DownArrow.svg'/></button>
+        </VoteHolder>
+        <div style={{ flexDirection: "row", flex: 8, paddingTop: '11px', paddingLeft: '10px' }}>
+          <div style={{color:'#4A4A4A'}}>{this.props.username}</div>
+          <div style={{color:'#C5C5C5', fontSize:'.8em'}}><img style={{width:'1em', marginRight:'5px'}} src='images/Clock.svg'/>{time}</div>
+          <div>{this.state.element}</div>
+          {/*<div className="credibility" />*/}
         </div>
       </div>
     );
   }
 }
+
+const VoteHolder = styled.div`
+  flex:1;
+  width: 3em;
+`;
 
 export default FeedItem;
