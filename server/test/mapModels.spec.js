6b096e17f7c756e52c21956f2269fa0be80247b1
Alex Seiden <alex.seiden@gmail.com>
2017-06-14 19:06:35 -0700

    (feat) test pin voting

--- server/test/mapModels.spec.js
+++ server/test/mapModels.spec.js
@@ -55,7 +55,7 @@ describe('Map model tests', function() {
     });
   });
 
-  it('post pins to the map', function(done) {
+  it('should post pins to the map', function(done) {
     map.postPin({
       map_id: 1,
       text: 'the test pin',
@@ -70,6 +70,29 @@ describe('Map model tests', function() {
       expect(data[0].map_id).to.equal(1);
       expect(data[0].text).to.equal('the test pin');
       done();
+    });
+  });
+
+  it('should vote on pins', function(done) {
+    map.postPin({
+      map_id: 1,
+      text: 'the test pin',
+      latitude: 0,
+      longitude: 0,
+      user_id: 1,
+      time: 0
+    },
+    function(err, data) {
+      map.insertPinVote({
+        raterId: 1,
+        pinId: 1,
+        polarity: 1
+      },
+      function(err, data) {
+        expect(err).to.equal(null);
+        expect(data).to.equal(1);
+        done();
+      });
     });
   });
 });
